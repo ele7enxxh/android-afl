02c8a70252757938aa96e762a6c151e3d9c303af
ele7enxxh <ele7enxxh@qq.com>
2016-09-05 21:56:01 +0800

    add android-test

---
+++ android-test/Android.mk
@@ -0,0 +1,78 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := crash.c
+LOCAL_MODULE := crash
+
+# test afl-gcc/afl-g++ on ARM/Android
+# to build: TEST_GCC_ARM=true mm -B
+ifeq ($(TEST_GCC_ARM), true)
+export AFL_CC := $(TARGET_CC)
+export AFL_AS := $(TARGET_TOOLS_PREFIX)as
+LOCAL_CLANG := false
+LOCAL_CC := afl-gcc
+include $(BUILD_EXECUTABLE)
+endif
+
+# test afl-gcc/afl-g++ on host
+# to build: TEST_GCC_HOST=true mm -B
+ifeq ($(TEST_GCC_HOST), true)
+export AFL_CC=$(HOST_CC)
+export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
+LOCAL_CLANG := false
+LOCAL_CC := afl-gcc
+include $(BUILD_HOST_EXECUTABLE)
+endif
+
+# test afl-clang/afl-clang++ on ARM/Android
+# to build: TEST_CLANG_ARM=true mm -B
+ifeq ($(TEST_CLANG_ARM), true)
+export AFL_CC=$(CLANG)
+export AFL_AS=$(TARGET_TOOLS_PREFIX)as
+LOCAL_CLANG := true
+LOCAL_CC := afl-clang
+include $(BUILD_EXECUTABLE)
+endif
+
+# test afl-clang/afl-clang++ on host
+# to build: TEST_CLANG_HOST=true mm -B
+ifeq ($(TEST_CLANG_HOST), true)
+export AFL_CC=$(CLANG)
+export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
+LOCAL_CLANG := true
+LOCAL_CC := afl-clang
+include $(BUILD_HOST_EXECUTABLE)
+endif
+
+# test afl-clang-fast/afl-clang-fast++ on ARM/Android
+# to build: TEST_CLANG_FAST_ARM=true mm -B
+ifeq ($(TEST_CLANG_FAST_ARM), true)
+export AFL_CC=/usr/bin/clang-3.8
+LOCAL_CLANG := true
+LOCAL_CC := afl-clang-fast
+LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt.o
+include $(BUILD_EXECUTABLE)
+endif
+
+# test afl-clang-fast/afl-clang-fast++ on 64bit host
+# to build: TEST_CLANG_FAST_HOST=true mm -B
+ifeq ($(TEST_CLANG_FAST_HOST), true)
+export AFL_CC=/usr/bin/clang-3.8
+LOCAL_MULTILIB := 64
+LOCAL_CLANG := true
+LOCAL_CC := afl-clang-fast
+LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-64.o
+include $(BUILD_HOST_EXECUTABLE)
+endif
+
+# test afl-clang-fast/afl-clang-fast++ on 32bit host
+# to build: TEST_CLANG_FAST_HOST_32=true mm -B
+ifeq ($(TEST_CLANG_FAST_HOST_32), true)
+export AFL_CC=/usr/bin/clang-3.8
+LOCAL_MULTILIB := 32
+LOCAL_CLANG := true
+LOCAL_CC := afl-clang-fast
+LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-32.o
+include $(BUILD_HOST_EXECUTABLE)
+endif
---
+++ android-test/crash.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+  char buf[5];
+
+  memset(buf, 5, 0);
+
+  read(0, buf, 5);
+
+  if (buf[0] == 'c')
+  	if (buf[1] == 'r')
+  		if (buf[2] == 'a')
+  			if (buf[3] == 's')
+  				if (buf[4] == 'h')
+  					abort();
+
+  return 0;
+}
