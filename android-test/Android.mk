9784271f03609aaefdaf075b522429a16dc443c5
ele7enxxh <ele7enxxh@qq.com>
2016-09-06 10:29:28 +0800

    update android-test

--- android-test/Android.mk
+++ android-test/Android.mk
@@ -9,9 +9,11 @@ LOCAL_MODULE := crash
 # to build: TEST_GCC_ARM=true mm -B
 ifeq ($(TEST_GCC_ARM), true)
 export AFL_CC := $(TARGET_CC)
+# expoet AFL_CXX := $(TARGET_CXX)
 export AFL_AS := $(TARGET_TOOLS_PREFIX)as
 LOCAL_CLANG := false
 LOCAL_CC := afl-gcc
+# LOCAL_CXX := afl-g++
 include $(BUILD_EXECUTABLE)
 endif
 
@@ -19,9 +21,11 @@ endif
 # to build: TEST_GCC_HOST=true mm -B
 ifeq ($(TEST_GCC_HOST), true)
 export AFL_CC=$(HOST_CC)
+# export AFL_CXX=$(HOST_CXX)
 export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := false
 LOCAL_CC := afl-gcc
+# LOCAL_CXX := afl-g++
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
@@ -29,9 +33,11 @@ endif
 # to build: TEST_CLANG_ARM=true mm -B
 ifeq ($(TEST_CLANG_ARM), true)
 export AFL_CC=$(CLANG)
+# export AFL_CXX=$(CLANG_CXX)
 export AFL_AS=$(TARGET_TOOLS_PREFIX)as
 LOCAL_CLANG := true
 LOCAL_CC := afl-clang
+# LOCAL_CC := afl-clang++
 include $(BUILD_EXECUTABLE)
 endif
 
@@ -39,9 +45,11 @@ endif
 # to build: TEST_CLANG_HOST=true mm -B
 ifeq ($(TEST_CLANG_HOST), true)
 export AFL_CC=$(CLANG)
+# export AFL_CXX=$(CLANG_CXX)
 export AFL_AS=$(HOST_TOOLCHAIN_PREFIX)as
 LOCAL_CLANG := true
 LOCAL_CC := afl-clang
+# LOCAL_CC := afl-clang++
 include $(BUILD_HOST_EXECUTABLE)
 endif
 
@@ -49,8 +57,10 @@ endif
 # to build: TEST_CLANG_FAST_ARM=true mm -B
 ifeq ($(TEST_CLANG_FAST_ARM), true)
 export AFL_CC=/usr/bin/clang-3.8
+# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_CLANG := true
 LOCAL_CC := afl-clang-fast
+# LOCAL_CXX := afl-clang-fast++
 LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt.o
 include $(BUILD_EXECUTABLE)
 endif
@@ -59,9 +69,11 @@ endif
 # to build: TEST_CLANG_FAST_HOST=true mm -B
 ifeq ($(TEST_CLANG_FAST_HOST), true)
 export AFL_CC=/usr/bin/clang-3.8
+# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_MULTILIB := 64
 LOCAL_CLANG := true
 LOCAL_CC := afl-clang-fast
+# LOCAL_CXX := afl-clang-fast++
 LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-64.o
 include $(BUILD_HOST_EXECUTABLE)
 endif
@@ -70,9 +82,11 @@ endif
 # to build: TEST_CLANG_FAST_HOST_32=true mm -B
 ifeq ($(TEST_CLANG_FAST_HOST_32), true)
 export AFL_CC=/usr/bin/clang-3.8
+# export AFL_CXX=/usr/bin/clang++-3.8
 LOCAL_MULTILIB := 32
 LOCAL_CLANG := true
 LOCAL_CC := afl-clang-fast
+# LOCAL_CXX := afl-clang-fast++
 LOCAL_LDFLAGS := $(HOST_OUT)/afl/afl-llvm-rt-host-32.o
 include $(BUILD_HOST_EXECUTABLE)
 endif
